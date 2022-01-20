3d84c767705f039410dbfb62b5b22b17f0d3af36
Cody Grogan <cod.grogan@gmail.com>
2022-01-20 16:18:11 +0800

    added react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import SchoolManager from './components/SchoolManager';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/school' element={<SchoolManager/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
