a7a911e686f547e19927d4855019892f5b480523
Cody Grogan <cod.grogan@gmail.com>
2022-01-23 13:41:21 +0800

    formatting for school page

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+
+function Navbar(props: any){
+    return(
+        <div>this is a navbar</div>
+    )
+}
+
+export default Navbar;
--- client/src/components/SchoolManager.tsx
+++ client/src/components/SchoolManager.tsx
@@ -1,18 +1,29 @@
+import {useState, useEffect} from 'react';
+
 
 function SchoolManager(props: any){
+
+
+
     return(
         <div>
             school manager
-            <div className="row">
-                <div className="col-2">The list of classes will go here</div>
+            <div className="row school-section">
+                <div className="col-2 list-box"><ul><li>The list of classes will go here</li><li>The list of classes will go here</li><li>The list of classes will go here</li><li>The list of classes will go here</li>
+                <li>The list of classes will go here</li><li>The list of classes will go here</li>
+                <li>The list of classes will go here</li><li>The list of classes will go here</li>
+                <li>The list of classes will go here</li><li>The list of classes will go here</li>
+                
+                
+                </ul></div>
                 <div className="col-10">this will quick display information about the selected class</div>
             </div>
 
-            <div className="row">
+            <div className="row school-section">
                 <div className="col-2">The list of teachers will go here</div>
                 <div className="col-10">this will quick display information about the selected teacher</div>
             </div>
-            <div className="row">
+            <div className="row school-section">
                 <div className="col-2">The list of students will go here</div>
                 <div className="col-10">this will quick display information about the selected student</div>
             </div>
