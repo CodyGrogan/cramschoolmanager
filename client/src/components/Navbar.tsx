b5e94a1941e6851d5f03b99be6e09418beef5c2e
Cody Grogan <cod.grogan@gmail.com>
2022-01-26 12:06:38 +0800

    added modal to student manager, form for new student

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -12,12 +12,16 @@ function Navbar(props: any){
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#"><Link to='/'>Home</Link></a>
+          <Link className="nav-link active" to='/'>Home</Link>
         </li>
         
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#"><Link to='/school'>School</Link></a>
+          <Link className="nav-link active" to='/school'>School</Link>
         </li>
+        <li className="nav-item">
+          <Link className="nav-link active" to='/student'>Students</Link>
+        </li>
+       
        
       </ul>
      
--- client/src/components/StudentManager.tsx
+++ client/src/components/StudentManager.tsx
@@ -23,9 +23,12 @@ function StudentManager(props: any){
     [props.school])
     
     return(
+        <div>
+             <Navbar/>
+             Student Manager
+
         <div className="school-section">
-            <Navbar/>
-            Student Manager
+           
             <table className="table table-stripped">
             <thead>
                 <th>Name</th>
@@ -42,9 +45,59 @@ function StudentManager(props: any){
             </table>
 
             <div className="centerButton">
-            <button className="btn btn-primary">Add Student</button>
+            <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#studentModal">Add Student</button>
             </div>
 
+            
+      
+            <div className="modal fade" id="studentModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Add Student</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                   
+                    <div className="mb-3">
+                        <label  className="form-label">Name</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        
+                    </div>
+
+                    <div className="mb-3">
+                        <label  className="form-label">Phone#</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        
+                    </div>
+                    <div className="mb-3">
+                        <label  className="form-label">Parent 1</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        
+                    </div>
+                    <div className="mb-3">
+                        <label  className="form-label">Parent 2</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        
+                    </div>
+                    <div className="mb-3">
+                        <label  className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        
+                    </div>
+                       
+                   
+                
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Add Student</button>
+                </div>
+                </div>
+            </div>
+            </div>
+
+        </div>
         </div>
     )
 }
