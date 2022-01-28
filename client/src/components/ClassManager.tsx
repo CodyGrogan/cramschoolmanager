380c7ac9db0c0c8d7e14fd32fc26b2208b7f94b2
Cody Grogan <cod.grogan@gmail.com>
2022-01-28 15:49:12 +0800

    fixed accordion bug in class manager

--- client/src/components/ClassManager.tsx
+++ client/src/components/ClassManager.tsx
@@ -168,12 +168,12 @@ function ClassManager(props: any){
                     </div>
 
                     <div className="accordion-item">
-                        <h2 className="accordion-header" id="flush-headingTwo">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                        <h2 className="accordion-header" id="flush-headingThree">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseTwo">
                             Lessons
                         </button>
                         </h2>
-                        <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                        <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body">
                                 <div >The list of lessons will go here</div>
                                 <div >this will quick display information about the selected lesson.
@@ -187,12 +187,12 @@ function ClassManager(props: any){
                     </div>
 
                     <div className="accordion-item">
-                        <h2 className="accordion-header" id="flush-headingTwo">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                        <h2 className="accordion-header" id="flush-headingFour">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseTwo">
                             Assignments
                         </button>
                         </h2>
-                        <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                        <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingfour" data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body">
 
                             <div >The list of assignments will go here</div>
