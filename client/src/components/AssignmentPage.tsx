89e7082e9e04329e5569c4b33f4c6a69904c12d9
Cody Grogan <cod.grogan@gmail.com>
2022-02-16 13:37:20 +0800

    Assignment Page displays assignment data

--- client/src/components/AssignmentPage.tsx
+++ client/src/components/AssignmentPage.tsx
@@ -1,9 +1,59 @@
 import Navbar from "./Navbar";
 import {useParams} from 'react-router-dom';
+import School from "../classes/School";
+import { useEffect, useState } from "react";
+import Lesson from "../classes/Lesson";
+import Assignment from "../classes/Assignment";
 
 function AssignmentPage(props: any){
 
     let { id, classname } = useParams();
+    const [assignment, setAssignment] = useState<Assignment>();
+    const [assignmentJSX, setAssignmentJSX] = useState<JSX.Element[]>([]);
+
+    let school: School = props.school;
+
+    useEffect(()=>{
+        let school: School = props.school;
+        let index = school.classList.findIndex(obj => obj.name == classname);
+        if (index >=0){
+        let lessonIndex = school.classList[index].assignmentList.findIndex(obj => obj.name == id);
+            if (lessonIndex >= 0){
+                setAssignment(school.classList[index].assignmentList[lessonIndex]);
+            }
+        }
+    },
+    []);
+
+    useEffect(()=>{
+        let school: School = props.school;
+     
+        let index = school.classList.findIndex(obj => obj.name == classname);
+        if (index >=0){
+        let lessonIndex = school.classList[index].assignmentList.findIndex(obj => obj.name == id);
+            if (lessonIndex >= 0){
+                setAssignment(school.classList[index].assignmentList[lessonIndex]);
+            }
+        }
+    },
+    [props.school]);
+    
+    useEffect(()=>{
+        buildJSX();
+    },
+    [assignment])
+
+    function buildJSX(){
+
+        let newJSX = <div>
+            {assignment?.name};
+            {assignment?.duedate};
+            
+        </div>;
+
+        setAssignmentJSX([newJSX]);
+
+    }
 
     return(
         <div>
@@ -12,6 +62,8 @@ function AssignmentPage(props: any){
             Assignment Page
             {id} <br/>
             {classname}
+
+            {assignmentJSX}
          
         </div>
     );
