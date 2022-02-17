aabedcf91d33e1378f91acab3d7ed404899cba98
Cody Grogan <cod.grogan@gmail.com>
2022-02-17 16:53:18 +0800

    update lesson page info works

--- client/src/components/LessonPage.tsx
+++ client/src/components/LessonPage.tsx
@@ -3,6 +3,8 @@ import {useParams} from 'react-router-dom';
 import Lesson from '../classes/Lesson';
 import School from '../classes/School';
 import Navbar from "./Navbar";
+import EditToast from './EditToast';
+import {getAuth} from 'firebase/auth'
 
 //this will use the parameters to query the database
 
@@ -12,6 +14,7 @@ function LessonPage(props: any){
     let defLesson = new Lesson('unknown', 0, ['unknown'], 'unknown', 'unknown', 'no plan');
     
     const [lesson, setLesson] = useState<Lesson>(defLesson);
+    const [hideToast, setHideToast] = useState<boolean>(false);
 
     useEffect(()=>{
 
@@ -55,6 +58,86 @@ function LessonPage(props: any){
     },
     [props.school]);
 
+    
+    useEffect(()=>{
+        if (hideToast == true){
+        setHideToast(false);
+       
+        }
+    },
+    [hideToast]);
+
+
+    async function editSchool(school: School){
+
+        let user = getAuth().currentUser;
+        if (user){
+        console.log('adding school info to database');
+        let jsonstring = JSON.stringify(school);
+        let postpath: string = '/editschool';
+
+        fetch(postpath, {
+            method: 'PUT', 
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: jsonstring,
+        })
+        .then(response => {
+            console.log(response)
+            showToast();
+            })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+        
+        }
+        else{
+            console.log('user not signed in, cannot edit')
+        }
+
+    }
+
+    function showToast(){
+        let toast = document.getElementById('editToast') as HTMLElement;
+        toast.hidden = false;
+        setHideToast(true);
+
+
+    }
+
+    function updateLesson(){
+        let newDate = (document.getElementById('dateInput') as HTMLInputElement).value;
+        let newLessonPlan = (document.getElementById('lessonPlanInput') as HTMLInputElement).value;
+
+        let school: School = props.school;
+        let allLessonsArr = [];
+        let classList = school.classList;
+        let index = classList.findIndex(obj => obj.name === classname);
+        if (index >=0){
+            let lessonList = classList[index].lessonList;
+            let lessonIndex = lessonList.findIndex(obj => obj.lessonid === id);
+            if (lessonIndex >=0){
+                if (newDate != '' && newDate != null){
+                lessonList[lessonIndex].date = newDate;
+                }
+                if (newLessonPlan != '' && newLessonPlan != null){
+                lessonList[lessonIndex].lessonplan = newLessonPlan;
+
+                }
+           
+            editSchool(school);
+           
+            setLesson(lessonList[lessonIndex])
+
+
+        }
+    }
+
+
+
+    }
+
 
 
     return(
@@ -88,8 +171,8 @@ function LessonPage(props: any){
                                 <div className="inputBox container">
                                     <div className="col-sm">
                                         <div className="inputDiv"> <label className="form-label">Date:</label><input className='form-control' id='dateInput' type={'date'}></input></div> 
-                                        <div className="inputDiv"> <label className="form-label">Parent 1:</label><textarea className='form-control' id='lessonPlanInput'placeholder={'Lesson Plan here'}></textarea></div> 
-                                            <button className="btn btn-primary">Edit</button>
+                                        <div className="inputDiv"> <label className="form-label">Lesson Plan:</label><textarea className='form-control' id='lessonPlanInput'placeholder={'Lesson Plan here'}></textarea></div> 
+                                            <button onClick={()=>updateLesson()} className="btn btn-primary">Edit</button>
                                             
                                             </div> 
 
@@ -108,6 +191,7 @@ function LessonPage(props: any){
                         </div>
                     </div>
                 </div>
+                <EditToast hideToast={hideToast}/>
             </div>
 
         
