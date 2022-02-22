b7fb6cde254113618ac360cc0a67426436a15188
Cody Grogan <cod.grogan@gmail.com>
2022-02-22 17:27:47 +0800

    home animations finished

--- client/src/components/HomeAnimation.js
+++ client/src/components/HomeAnimation.js
@@ -1,4 +1,4 @@
-import {ScrollContainer, ScrollPage, Animator, batch, Fade, Sticky, MoveOut, ZoomIn, ZoomOut, FadeIn, StickyIn} from 'react-scroll-motion';
+import {ScrollContainer, ScrollPage, Animator, batch, Fade, Sticky, MoveOut, ZoomIn, ZoomOut, FadeIn, StickyIn, MoveIn} from 'react-scroll-motion';
 
 const ZoomInScrollOut = batch(Sticky(), FadeIn(), ZoomIn())
 
@@ -16,22 +16,26 @@ function HomeAnimation(props){
         <ScrollContainer snap="mandatory" >
             <ScrollPage page={0}>
         
-        <div className='scrollContainer'>
+        <div className='scrollPageContainer'>
             <Animator animation={batch(Fade(),  MoveOut(0, -200))}>
-                <div>
-                <h2>Organize your School</h2>
-                <img width={'300px'} src={stockPhoto1} />
+                <div className='intro-div'>
+                <h2>Welcome to Cram School Manager</h2>
+                <img className='intro-img' width={'250px'} src={stockPhoto1} /><br/>
                 
+                </div>
+                <div className='centerButton'>
+                <button type="button" className="btn btn-primary loginButton" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#signUpModal"><h1>Try Now!</h1></button>
                 </div>
             </Animator>
 
             <Animator animation={batch(Fade(),  MoveOut(0, 200))}>
                 <div>
-                <button type="button" className="btn btn-primary loginButton" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#signUpModal"><h1>Try Now!</h1></button>
 
                 
                 </div>
             </Animator>
+
+            
             </div>
 
 
@@ -42,21 +46,23 @@ function HomeAnimation(props){
 
         <ScrollPage page={1}>
 
-            <div className='scrollContainer'>
+            <div className='scrollPageContainer'>
         
-            <Animator animation={batch(Fade(), MoveOut(200, 200))}>
-                <div>
+            <Animator animation={batch(Fade(), MoveOut(0, -200))}>
+                <div className='intro-div'>
                 <h2>Simplify gradebook management and focus on what matters</h2>
            
                 </div>
 
             </Animator>
-            <Animator animation={batch(Fade(), MoveOut(0, -400))}>
-                <div>
-                <img width={'300px'} src={stockPhoto2} />
+            <Animator animation={batch(Fade(), MoveIn(0, 200), MoveOut(0, -200))}>
+                <div className='img-back'>
+                <img className='intro-img img-back' width={'250px'} src={stockPhoto2} />
                 </div>
 
             </Animator>
+
+            
             
             </div>
 
@@ -66,10 +72,12 @@ function HomeAnimation(props){
 
         <ScrollPage page={2}>
         
-        <Animator animation={batch(Fade(), MoveOut(200, -200))}>
-            <div>
-            <h2>Provide easy access to lesson plans</h2>
-            <img height={'300px'} src={stockPhoto3} />
+        <Animator animation={batch(Fade(), MoveIn(-200, 0), Sticky(), MoveOut(200, 0))}>
+            <div className='scrollPageContainer'>
+                <div className='intro-div'>
+                <h2>Provide easy access to lesson plans</h2>
+                <img className='intro-img' height={'250px'} src={stockPhoto3}  />
+                </div>
             </div>
         </Animator>
         </ScrollPage>
