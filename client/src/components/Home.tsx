544bb351983bd4236e97bce58503118bdf9142b9
Cody Grogan <cod.grogan@gmail.com>
2022-03-10 17:47:26 +0800

    implementing aos

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,15 +3,16 @@ import Navbar from "./Navbar";
 import GradeBarChart from "./GradeBarChart";
 import GradeAttendChart from "./GradeAttendChart";
 import HomeAnimation from "./HomeAnimation";
-
+import Aos from "aos";
 
 
 function Home(props: any){
 
- 
+    Aos.init();
    
     return(
         <div>
+            <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
             <Navbar setSchool={props.setSchool} homepage={true}/>
         
             <HomeAnimation/>
--- client/src/components/HomeAnimation.js
+++ client/src/components/HomeAnimation.js
@@ -1,6 +1,4 @@
-import {ScrollContainer, ScrollPage, Animator, batch, Fade, Sticky, MoveOut, ZoomIn, ZoomOut, FadeIn, StickyIn, MoveIn} from 'react-scroll-motion';
-
-const ZoomInScrollOut = batch(Sticky(), FadeIn(), ZoomIn())
+import Aos from 'aos'
 
 const stockPhoto1 = './images/StockPhoto1.jpg'
 const stockPhoto2 = './images/StockPhoto2.jpg'
@@ -11,14 +9,10 @@ function HomeAnimation(props){
 
 
     return(
-     
+     <div>
 
-        <ScrollContainer snap="mandatory" >
-            <ScrollPage page={0}>
-        
-        <div className='scrollPageContainer'>
-            <Animator animation={batch(Fade(),  MoveOut(0, -200))}>
-                <div className='intro-div'>
+       
+                <div  className='intro-div'>
                 <h2>Welcome to Cram School Manager</h2>
                 <img className='intro-img' width={'250px'} src={stockPhoto1} /><br/>
                 
@@ -26,18 +20,9 @@ function HomeAnimation(props){
                 <div className='centerButton'>
                 <button type="button" className="btn btn-primary loginButton" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#signUpModal"><h1>Try Now!</h1></button>
                 </div>
-            </Animator>
-
-            <Animator animation={batch(Fade(),  MoveOut(0, 200))}>
-                <div>
-
-                
-                </div>
-            </Animator>
-      
+            
 
             
-            </div>
 
             <br/>
         <div className='centerButton homepage-div'>
@@ -56,50 +41,37 @@ function HomeAnimation(props){
        </div>
 
 
-            </ScrollPage>
-
             
 
+       <br/>       <br/>       <br/>
+    
 
-        <ScrollPage page={1}>
+            <div data-aos='fade-left' >
 
-            <div className='scrollPageContainer'>
-
-            <Animator animation={batch(Fade(), MoveIn(0, -200), MoveOut(0, 200))}>
                 <div>
                 <img className='intro-img img-back' width={'250px'} src={stockPhoto2} />
                 </div>
 
-            </Animator>
         
-            <Animator animation={batch(Fade(), MoveOut(0, -200))}>
                 <div className='intro-div'>
                 <h2>Simplify gradebook management and focus on what matters</h2>
            
                 </div>
 
-            </Animator>
            
 
             
             
             </div>
 
-        </ScrollPage>
 
     
-
-        <ScrollPage page={2}>
-        
-        <Animator animation={batch(Fade(), MoveIn(-200, 0), Sticky(), MoveOut(200, 0))}>
-            <div className='scrollPageContainer'>
+       <div  data-aos='fade-right' >
                 <div className='intro-div'>
                 <h2>Provide easy access to lesson plans</h2>
                 <img className='intro-img' height={'250px'} src={stockPhoto3}  />
                 </div>
-            </div>
-        </Animator>
-        </ScrollPage>
+        </div>
 
         
 
@@ -107,18 +79,13 @@ function HomeAnimation(props){
 
  
 
-        <ScrollPage page={3}>
-        
-        <Animator animation={ZoomInScrollOut}>
             
-        <button type="button" className="btn btn-primary loginButton" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#signUpModal"><h1>Try Now!</h1></button>
+        <button data-aos='fade-left'  type="button" className="btn btn-primary loginButton" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#signUpModal"><h1>Try Now!</h1></button>
 
-        </Animator>
-        </ScrollPage>
-    
 
-        </ScrollContainer>
 
+
+        </div>
       
     )
 }
