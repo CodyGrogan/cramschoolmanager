9e97f19d49f3971bf28b87f2a72882ac5aee876f
Cody Grogan <cod.grogan@gmail.com>
2022-03-16 18:13:20 +0800

    get school data sends token with requested uid

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -154,8 +154,15 @@ function Navbar(props: any){
 
     console.log('getting school data')
     let uid = getAuth().currentUser?.uid;
+    
+    let token = await getAuth().currentUser?.getIdToken();
+
+    let UIDToken = [uid, token];
+
     console.log('uid is ' + uid);
-    let obj = JSON.stringify({uid});
+    let obj = JSON.stringify({UIDToken});
+    
+
     let postpath: string = '/getschoolinfo';
 
     fetch(postpath, {
