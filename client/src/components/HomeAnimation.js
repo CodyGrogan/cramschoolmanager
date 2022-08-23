a72952366f7507ac9ed0744c9388c4c21466beba
Cody.Grogan <cod.grogan@gmail.com>
2022-08-23 15:22:04 -0400

    removed animations from home page

--- client/src/components/HomeAnimation.js
+++ client/src/components/HomeAnimation.js
@@ -29,21 +29,8 @@ function HomeAnimation(props){
 
                     </div>
 
-                    <div className='centerButton'>
-                    
-                    <p >Scroll Down to Learn More</p> <br/>
-                    
-                    </div>
-
-                    <div className='centerButton'>
-                    
-                    <div>
-                    <span className='homepage-arrow material-icons'>
-                    expand_more
-                    </span>
-                    </div>
-                    </div>
-
+                  
+               
                 
                 </div>
     
@@ -55,50 +42,18 @@ function HomeAnimation(props){
      
 
 
-            <div className='scrollPageContainer' >
-
-                <div data-aos='fade-left' >
-                <img className='intro-img img-back' width={'250px'} src={stockPhoto2} />
-                </div>
-
-        
-                <div className='intro-div' data-aos='fade-right' >
-                <h2>Simplify gradebook management and focus on what matters</h2>
            
-                </div>
-
-           
-
-            
-            
-            </div>
 
 
     
-       <div className='scrollPageContainer'  data-aos='fade-right' >
-                <div className='intro-div'>
-                <h2>Provide easy access to lesson plans</h2>
-                <img className='intro-img' height={'250px'} src={stockPhoto3}  />
-                </div>
-        </div>
-
+    
         
 
       
 
  
-        <div className='scrollPageContainer' data-aos='fade-left'  >
-            
-        <button type="button" className="btn btn-primary loginButton" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#signUpModal"><h1>Try Now!</h1></button>
-
-        </div>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-
+    
+     
 
         </div>
       
